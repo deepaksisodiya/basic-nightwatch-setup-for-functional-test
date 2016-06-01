7c879701d521702ee01a7f5a80668c9183abcc26
deepak sisodiya <deepak.sisodiya@courgetteconcepts.com>
2016-06-01 19:13:12 +0530

    functional test configuration is setup and running using nightwatch.js

---
+++ e2e/test.js
@@ -0,0 +1,21 @@
+/**
+ * Created by deepaksisodiya on 01/06/16.
+ */
+
+
+module.exports = {
+
+  'should go to the localhost at 3000 port': (browser) => {
+
+    browser
+      .url('http://localhost:3000')
+      .useXpath()
+      .waitForElementVisible('//body', 2000)
+      .assert.containsText('//pre[text()="Hello from koa.js"]', 'Hello from koa.js')
+      .pause(2000)
+      .end();
+
+  }
+
+
+};
